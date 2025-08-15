2b7449342d26645682c2641d3a692f5ce21df90b
Liandro  Bulegon <lbulegon@gmail.com>
2025-08-15 03:11:20 -0300

    add

--- app_eventos/views.py
+++ app_eventos/views.py
@@ -1,3 +1,6 @@
+# views.py
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def health_check(request):
+    return HttpResponse("✅ Aplicação rodando OK 🚀")
--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib import admin
+
+from app_eventos import views  
 
 urlpatterns = [
+    path("", views.health_check, name="health_check"),
     path("admin/", admin.site.urls),
 
     # JWT
     path("auth/jwt/create/", TokenObtainPairView.as_view(), name="jwt-create"),
     path("auth/jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
+
+    
+
+]
 
     # API v1 (negócio + auth de signup)
    # path("api/", include("api_v01.urls")),
